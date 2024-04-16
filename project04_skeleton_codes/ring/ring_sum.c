06b4d110de248d24ea16572f45cce551ec7b738c
Timo Schwab <tischwab@ethz.ch>
2024-04-16 10:07:13 +0200

    fixed typo and changed to true ring

--- project04_skeleton_codes/ring/ring_sum.c
+++ project04_skeleton_codes/ring/ring_sum.c
@@ -10,18 +10,19 @@ int main(int argc, char *argv[]) {
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
   // IMPLEMENT: Ring sum algorithm
-  int sum = rank; // initialize sum
+  int sum = 0; // initialize sum
   printf("Process %i: Sum = %i\n", rank, sum);
 
   // ring sum
   int recvBuf[size];
   recvBuf[0] = rank;
-  for(int i = 1; i < size; ++i){
-    MPI_Send(&rank, 1, MPI_INT, (rank+i)%size, 42, MPI_COMM_WORLD);
+  for(int i = 1; i < size+1; ++i){
+    MPI_Send(&recvBuf[i-1], 1, MPI_INT, (rank+1)%size, 42, MPI_COMM_WORLD);
     MPI_Recv(&recvBuf[i], 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     sum += recvBuf[i];
-    printf("Process %i: Sum = %i\n", rank, sum);
   }
+
+  printf("Process %i: Sum = %i\n", rank, sum);
 
   // Finalize MPI
   MPI_Finalize();

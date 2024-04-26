786ee834b9f0d3d0effc8c37a68d8feaa4772e30
Timo Schwab <tischwab@ethz.ch>
2024-04-26 17:03:11 +0200

    submit script for reproducability

--- project04_skeleton_codes/ring/submit.sh
+++ project04_skeleton_codes/ring/submit.sh
@@ -3,9 +3,16 @@
 #SBATCH --job-name=ringsum
 #SBATCH --output=out/ringsum-%j.out
 #SBATCH --error=out/ringsum-%j.err
-#SBAtCH --ntasks=6
-#SBATCH --cpus-per-task=1
+#SBATCH --ntasks=16
 #SBATCH --constraint=EPYC_7763
-#SBATCH --time=00:01:00
+#SBATCH --time=00:05:00
 
-mpirun ring_sum
+module load gcc openmpi
+
+make clean
+make
+
+mpirun -np 2 ./ring_sum
+mpirun -np 4 ./ring_sum
+mpirun -np 8 ./ring_sum
+mpirun -np 16 ./ring_sum

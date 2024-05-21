1fcc5ea2e876048df4f3369a2c6d487eeae374cd
lily <lwatanabe@ethz.ch>
2024-05-21 10:04:03 +0200

    results of runs

--- project06_skeleton_codes/poisson_petsc/scripts/strong_scaling_poisson_64.sh
+++ project06_skeleton_codes/poisson_petsc/scripts/strong_scaling_poisson_64.sh
@@ -5,13 +5,13 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --tasks-per-node=1
 #SBATCH --mem-per-cpu=2G
-#SBATCH --output=./../strong/poisson1-%j.out
-#SBATCH --error=./../strong/poisson1-%j.err
-#SBATCH --time=8:00:00
+#SBATCH --output=./../strong/poisson64-%j.out
+#SBATCH --error=./../strong/poisson64-%j.err
+#SBATCH --time=12:00:00
 #SBATCH --constraint=EPYC_7763
 
 for np in 1 2 4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64; do
     for rep in {1..5}; do
-        mpirun -np $np ./poisson -m 4096
+        mpirun -np $np ./../poisson -m 9192
     done
 done
